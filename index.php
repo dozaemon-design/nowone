df3e44a0cb065963809287f3f6e4e1b8b6870dbd
dozaemon-design <develop@nowone.jp>
2024-01-17 17:10:20 +0900

    github-test

--- index.php
+++ index.php
@@ -17,8 +17,8 @@
   </li>
 <?php endforeach; ?>
 </ul>
-
-<ul class="music_list">
+<?php if( in_category('movie') ): ?>
+<ul class="movie_list">
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
   <li>
       <?php
@@ -29,6 +29,22 @@
   </li>
 <?php endwhile; endif; ?>
 </ul>
+
+<?php elseif( in_category('music') ): ?>
+	<ul class="music_list">
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+  <li>
+		<a href="<?php the_field('music_link'); ?>">
+      <?php
+        $music_thumb = get_field('music_thumb');
+        if( $music_thumb ) { ?>
+       <img src="<?php the_field('music_thumb'); ?>" alt="">
+			<?php } ?>
+		</a>
+	</li>
+<?php endwhile; endif; ?>
+</ul>
+<?php endif; ?>
 </main>
 </div>
 <?php get_footer(); ?>
